246b63b8d0d5972939cdf821c161e74d74a6fd3c
wenyizag <wenyizag@outlook.com>
2018-05-25 21:17:59 -0400

    JS finished

---
+++ Day 15 - Upload/js/index.js
@@ -0,0 +1,56 @@
+
+var droppedFiles = false;
+var uploading = false;
+var fileName = '';
+var $dropzone = $('.dropzone');
+var $button = $('.upload-btn');
+var $syncing = $('.syncing');
+var $done = $('.done');
+var $bar = $('.bar');
+var timeOut;
+
+$dropzone.on('drag dragstart dragend dragover dragenter dragleave drop', function(e){
+	e.preventDefault();
+	e.stopPropagation();
+})
+
+	.on('dragover dragenter', function() {
+	$dropzone.addClass('is-dragover');
+})
+
+	.on('dragleave dragend drop', function(){
+	$dropzone.removeClass('is-dragover');
+})
+
+	.on('drop', function(e){
+	droppedFiles = e.originalEvent.dataTransfer.files;
+	fileName = droppedFiles[0]['name'];
+	$('.filename').html(fileName);
+	$('.dropzone .upload').hide();
+});
+
+$button.bind('click', function(){
+	startUpload();
+});
+
+$("input:file").change(function(){
+	fileName = $(this)[0].files[0].name;
+	$('.filename').html(fileName);
+	$('.dropzone .upload').hide();
+});
+
+function startUpload() {
+	if(!uploading && fileName != ''){
+		upload = true;
+		$button.html('Uploading...');
+		$dropzone.fadeOut();
+		$syncing.addClass('active');
+		$done.addClass('active');
+		$bar.addClass('active');
+		timeoutID = window.setTimeOut(showDone, 3200);
+	}
+}
+
+function showdone() {
+	$botton.html('Done');
+}
